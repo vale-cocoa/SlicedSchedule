213e9b8732bf2d0633fa4cbbaa8f7a438f51bd34
Valeriano Della Longa <valeriano.dellalonga@me.com>
2020-03-27 13:02:26 +0100

    Start working on Schedule conformance: implemented the schedule(matching: direction:) method.

---
+++ Sources/SlicedSchedule/SlicedSchedule+Schedule.swift
@@ -0,0 +1,92 @@
+//
+//  SlicedSchedule
+//  SlicedSchedule+Schedule.swift
+//  
+//  Created by Valeriano Della Longa on 27/03/2020.
+//  Copyright © 2020 Valeriano Della Longa. All rights reserved.
+//
+
+import Foundation
+import Schedule
+
+extension SlicedSchedule: Schedule {
+    var lowerBound: Date? { return self.elements.first?.start }
+    var upperBound: Date? { return self.elements.last?.end }
+    
+    public var isEmpty: Bool { return elements.isEmpty }
+    
+    public func contains(_ date: Date) -> Bool {
+        guard
+            !isEmpty,
+            date >= lowerBound!,
+            date <= upperBound!,
+            let _ = elements.last(where: { $0.contains(date) })
+            else { return false }
+        
+        return true
+    }
+    
+    public func schedule(matching date: Date, direction: CalendarCalculationMatchingDateDirection) -> Self.Element? {
+        guard !isEmpty else { return nil }
+        
+        switch direction {
+        case .on:
+            guard
+                date >= lowerBound!,
+                date <= upperBound!
+                else { return nil }
+            
+            return elements
+                .last(where: { $0.contains(date) })
+        
+        case .firstAfter:
+            guard date < elements.last!.start else { return nil }
+            
+            if date < lowerBound! { return elements.first }
+            
+            if
+                let idxOfOn = elements
+                    .firstIndex(where: { $0.contains(date) })
+            {
+                // There's an element containing the date…
+                // …we return next one!
+                // This is safe since we've guarded against
+                // date greater than or equal last element's start
+                return elements[idxOfOn + 1]
+            } else {
+                // No element contains the date,
+                // hence we return the first one whose start is
+                // greater than the date!
+                return elements
+                    .first(where: { $0.start > date })
+            }
+        
+        case .firstBefore:
+            guard date > elements.first!.end else { return nil }
+            
+            if date > upperBound! { return elements.last }
+            
+            if
+                let idxOfOn = elements
+                    .firstIndex(where: { $0.contains(date) })
+            {
+                // there's an element contaning the date…
+                // …we return the one before!
+                // This is safe since we've guarded against
+                // date smaller than or equal first element's end
+                return elements[idxOfOn - 1]
+            } else {
+                // No element contains the date,
+                // hence we return the last element whose end is smaller
+                // than date!
+                return elements
+                    .last(where: { $0.end < date })
+            }
+        }
+    }
+    
+    public func schedule(in dateInterval: DateInterval, queue: DispatchQueue?, then completion: @escaping (Result<[Self.Element], Error>) -> Void) {
+        
+    }
+    
+}
---
+++ Tests/SlicedScheduleTests/SliceScheduleScheduleTests.swift
@@ -0,0 +1,16 @@
+//
+//  SlicedScheduleTests
+//  SliceScheduleScheduleTests.swift
+//  
+//
+//  Created by Valeriano Della Longa on 27/03/2020.
+//  Copyright © 2020 Valeriano Della Longa. All rights reserved.
+//
+
+import XCTest
+@testable import SlicedSchedule
+import Schedule
+
+final class SliceScheduleScheduleTests: XCTestCase {
+    
+}
